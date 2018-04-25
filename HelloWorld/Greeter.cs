e51a3b7d69cba1b9839e7c2264ac12c5151983d6
opilane <opilane@HP-CZC6297BB0>
2018-04-25 10:32:16 +0300

    Add Greet class and Whom

---
+++ HelloWorld/Greeter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Greeter
+    {
+        public void Greet(string Whom)
+        {
+            Console.WriteLine("Hello " + Whom);
+        }
+    }
+}
